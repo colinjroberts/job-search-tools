f9e08d9611c1b1526a3a5bd93c50ff6a0d886bad
CJ <salinecjr@gmail.com>
2021-12-30 17:23:21 -0800

    Add working example of tabs

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # job-search-tools
 Tools for organizing and performing a job search
+
+## General Goals
+- Track companies I am interested in
+  - Company name, jobs website, people I may want to talk to, notes about products and languages, company's goals/mission, etc. 
+  - Track who I've talked to and when 
+  - Periodically scrape for new jobs and alert me when something is available I might like
+- Track jobs I'm interested in and steps in application process:
+  - Job title and description (perhaps parsed from website)
+  - Track when I applied and how, how far I got in the process, what decisions were made.
+- Generate somewhat tailored cover letters and resumes. 
+  - For each job entry I have, write about my accomplishments in a number of different ways
+  - Then when I need a resume or cover letter, generate it based on the tags for the role.
+    
+## User Story Goals:
+- I want to open my program and see a list of open positions posted by companies I've curated.
+- I want to see positions I am actively in the process of applying to and where I am in the process.
+- I want to not see (but be able to look up), roles I've not received.
+- I want a place to take notes about a company that I can later look up.
+- I want a repo of interview questions I've been asked from each company I can review and practice from.
+
+## End-to-end examples:
+1. Open app. Look at outstanding TODOs and dismiss any that have been completed. Run job scraper, go through newly found positions and save or dismiss them (dismissing means they will not show up again in future scrapes). For those saved, decide if I want to apply directly or try to contact people who work in the company for a chat first. Add outreach, practice, or little project tasks to TODO list. If I choose to reach out to someone, note who it is, their title, when I first reached out. When I talk to them, have spot for notes for company (with person as source) and notes specific to that person. 
+
+## TODO:
+- Milestone: Build manual version of E2E 1
+  - Sketch a usable design for system using curses with the following screens:
+    - Todo
+    - People
+    - Companies
+    - Jobs
+  - Build tables for people, companies, jobs, notes
+  - Try adding a few jobs
+- Milestone: Build job scraper
---
+++ main.py
@@ -0,0 +1,163 @@
+# main.py
+
+"""
+This program is an attempt to make a TUI for a few processes I find myself doing frequently
+while looking for a job. The general layout is expected to be as follows:
+
+One of the simpler screens will look something like this:
+ ┌──────────────────┐
+ │┌────────────────┐│
+ │├────────────────┤│
+ ││ ┌─┐ ┌─────────┐││
+ ││ │ │ │         │││
+ ││ └─┘ └─────────┘││
+ │└────────────────┘│
+ └──────────────────┘
+
+
+Outer/Top-most layer:
+ ┌──────────────────┐
+ │                  │
+ │  main_pile       │
+ │                  │
+ │                  │
+ │                  │
+ │                  │
+ └──────────────────┘
+
+Inside main_pile is:
+ ┌────────────────┐
+ │ tab_menu       │
+ ├────────────────┤
+ │                │
+ │ body_container │
+ │                │
+ │                │
+ └────────────────┘
+
+Next steps will involve adding more containers into body_container
+Inside body_container will be some kind of subdivision
+ ┌───────────┐  ┌─────────────────────────────┐
+ │  body_    │  │  body_main_window           │
+ │  side_    │  │                             │
+ │  bar      │  │                             │
+ │           │  │                             │
+ │           │  │                             │
+ │           │  │                             │
+ │           │  │                             │
+ │           │  │                             │
+ └───────────┘  └─────────────────────────────┘
+"""
+
+
+import urwid
+import csv
+
+"""
+For now, the companies(), contacts(), and jobs() functions read in csvs of text and return them
+as urwid Text objects. Later, these will likely become calls to whatever backend is used...SQLite 
+probably?
+"""
+def companies():
+    company_names = []
+    with open('data/companies.csv') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fieldnames = next(csvreader)
+
+        reading = True
+        while reading:
+            try:
+                company_names.append(next(csvreader)[1] + "\n")
+            except StopIteration:
+                reading = False
+
+    return urwid.Text(company_names)
+
+def contacts():
+    contact_names = []
+    with open('data/contacts.csv') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fieldnames = next(csvreader)
+
+        reading = True
+        while reading:
+            try:
+                contact_names.append(next(csvreader)[1] + "\n")
+            except StopIteration:
+                reading = False
+
+    return urwid.Text(contact_names)
+
+def jobs():
+    job_names = []
+    with open('data/jobs.csv') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fieldnames = next(csvreader)
+
+        reading = True
+        while reading:
+            try:
+                job_names.append(next(csvreader)[2] + "\n")
+            except StopIteration:
+                reading = False
+
+    return urwid.Text(job_names)
+
+
+def build_tab_menu(choices):
+    """Defines and builds tab_menu using provided choices
+    Probably doesn't need to be this convoluted.
+    """
+    cells = []
+    for item in choices:
+        button = urwid.Button(item)
+        urwid.connect_signal(button, 'click', on_tab_click, item)
+        cells.append(urwid.AttrMap(button, None, focus_map='reversed'))
+    return urwid.GridFlow(cells, 20, 2, 1, "left")
+
+
+def body_picker(button, choice):
+    """Function for directly changing the text in body_container"""
+    if choice == "Todo":
+        body_container.body = urwid.Text("Todo", 'left', 'clip')
+    elif choice == "Companies":
+        body_container.body = companies()
+    elif choice == "Contacts":
+        body_container.body = contacts()
+    elif choice == "Jobs":
+        body_container.body = jobs()
+    else:
+        body_container.body = urwid.Text("There must have been a mistake.", 'left', 'clip')
+
+
+def on_tab_click(button, choice):
+    """Callback function for changing main_body
+    :param button: not sure what this is for, but some first variable is expected
+    :param choice: user variable used for switch
+    :return: None. For now, this just calls the body_picker function which directly changes
+    """
+    body_picker(button, choice)
+
+
+def exit_program():
+    raise urwid.ExitMainLoop()
+
+
+def q_for_exit(key):
+    if key in ('q', 'Q'):
+        exit_program()
+
+if __name__ == "__main__":
+
+    # Build primary two subdivisions: tab_menu and body_container
+    tab_menu = build_tab_menu(['Todo', 'Jobs', 'Companies', 'Contacts'])
+    body_container = urwid.Filler(urwid.Text("default", 'left', 'clip'), "top")
+
+    # Arrange primary items into a Pile
+    main_pile = urwid.Pile([('pack', tab_menu), body_container])
+
+    mainloop = urwid.MainLoop(main_pile,
+                              palette=[('reversed', 'standout', '')],
+                              unhandled_input=q_for_exit(None))
+
+    mainloop.run()
